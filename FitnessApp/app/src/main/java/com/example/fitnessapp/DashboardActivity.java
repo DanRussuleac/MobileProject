bfeb28ec3efa1c9ba00361ffb8c5bd7f14d768e2
DanRussuleac <drussuleac@gmail.com>
2023-12-07 11:30:48 +0000

    permissions for step counter in foreground

--- FitnessApp/app/src/main/java/com/example/fitnessapp/DashboardActivity.java
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/DashboardActivity.java
@@ -1,18 +1,30 @@
 package com.example.fitnessapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import java.util.Locale;
 
 public class DashboardActivity extends AppCompatActivity implements SensorEventListener {
 
+    private static final int REQUEST_ACTIVITY_RECOGNITION_PERMISSION = 1;
+
     private TextView tvStepCounter;
+    private TextView tvStepProgressPercentage;
+    private ProgressBar pbStepProgress;
     private SensorManager sensorManager;
     private Sensor stepSensor;
     private boolean isSensorPresent;
@@ -23,31 +35,54 @@ public class DashboardActivity extends AppCompatActivity implements SensorEventL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        Intent serviceIntent = new Intent(this, StepCounterService.class);
-        startService(serviceIntent);
-
         tvStepCounter = findViewById(R.id.tvStepCounter);
+        tvStepProgressPercentage = findViewById(R.id.tvStepProgressPercentage);
+        pbStepProgress = findViewById(R.id.pbStepProgress);
         Button btnViewWorkoutLog = findViewById(R.id.btnViewWorkoutLog);
         Button btnSetGoals = findViewById(R.id.btnSetGoals);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // For Android 10 (API level 29) and above
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, REQUEST_ACTIVITY_RECOGNITION_PERMISSION);
+            } else {
+                startStepCounterService();
+            }
+        } else {
+            // For Android 9 (Pie, API level 28) and below
+            startStepCounterService();
+        }
+
+        btnViewWorkoutLog.setOnClickListener(view -> startActivity(new Intent(DashboardActivity.this, WorkoutLogActivity.class)));
+        btnSetGoals.setOnClickListener(view -> startActivity(new Intent(DashboardActivity.this, GoalsAchievementsActivity.class)));
+    }
+
+    private void startStepCounterService() {
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
             stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
             isSensorPresent = true;
+            startService(new Intent(this, StepCounterService.class));
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI);
         } else {
             tvStepCounter.setText("Step Counter Sensor not present!");
             isSensorPresent = false;
         }
+    }
 
-        btnViewWorkoutLog.setOnClickListener(view -> {
-            Intent intent = new Intent(DashboardActivity.this, WorkoutLogActivity.class);
-            startActivity(intent);
-        });
-
-        btnSetGoals.setOnClickListener(view -> {
-            Intent intent = new Intent(DashboardActivity.this, GoalsAchievementsActivity.class);
-            startActivity(intent);
-        });
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_ACTIVITY_RECOGNITION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startStepCounterService();
+            } else {
+                tvStepCounter.setText("Step Counter Permission not granted!");
+                isSensorPresent = false;
+            }
+        }
     }
 
     @Override
@@ -70,12 +105,19 @@ public class DashboardActivity extends AppCompatActivity implements SensorEventL
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor == stepSensor) {
             stepCount = (int) event.values[0];
-            tvStepCounter.setText(String.format("Steps: %d", stepCount));
+            updateStepCountDisplay(stepCount);
         }
+    }
+
+    private void updateStepCountDisplay(int steps) {
+        tvStepCounter.setText(String.format(Locale.US, "Steps: %d/10,000", steps));
+        int stepGoal = 10000;
+        pbStepProgress.setProgress(steps);
+        float percentage = (steps / (float) stepGoal) * 100;
+        tvStepProgressPercentage.setText(String.format(Locale.US, "%.2f%%", percentage));
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Can be used to respond to changes in sensor accuracy
     }
 }
--- FitnessApp/app/src/main/java/com/example/fitnessapp/StepCounterService.java
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/StepCounterService.java
@@ -1,38 +1,65 @@
 package com.example.fitnessapp;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.IBinder;
-
 
 public class StepCounterService extends Service implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor stepSensor;
+    private int totalStepsSinceReboot;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
             stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI);
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            startForegroundService();
         }
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        sensorManager.unregisterListener(this);
+    private void startForegroundService() {
+        String channelId = "step_counter_service_channel";
+        String channelName = "Step Counter Service";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    channelId,
+                    channelName,
+                    NotificationManager.IMPORTANCE_LOW);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        Intent notificationIntent = new Intent(this, DashboardActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, notificationIntent, 0);
+
+        Notification notification = new Notification.Builder(this, channelId)
+                .setContentTitle("Step Counter Active")
+                .setContentText("Counting your steps.")
+                .setSmallIcon(R.drawable.ic_launcher_foreground) // Use your own drawable icon
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1, notification);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // If your service gets terminated, it should restart with the last intent that was delivered to it.
-        return START_REDELIVER_INTENT;
+        return START_STICKY;
     }
 
     @Override
@@ -42,12 +69,26 @@ public class StepCounterService extends Service implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        // Handle sensor changes
+        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            totalStepsSinceReboot = (int) event.values[0];
+            saveSteps(totalStepsSinceReboot);
+        }
+    }
+
+    private void saveSteps(int steps) {
+        SharedPreferences prefs = getSharedPreferences("StepCounterPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("steps", steps);
+        editor.apply();
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Handle accuracy changes
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        sensorManager.unregisterListener(this);
     }
 }
-
